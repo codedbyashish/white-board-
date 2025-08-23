fbb81fc97ca4251bf46f9b97d383c7f6a2b610e4
ashishcode-stack <ashishkumarmaurya794@gmail.com>
2025-08-23 20:58:21 +0530

    intial refernece are created

---
+++ script.js
@@ -0,0 +1,18 @@
+// initial refernence
+
+let colorsRef=document.getElementsByClassName("colors")
+let canvas=document.getElementById("canvas")
+let backgroundbutton=document.getElementById("color-background")
+let colourbutton=document.getElementById("color-input")
+let clearbutton=document.getElementById("button-clear")
+let erasebutton=document.getElementById("button-erase")
+let pen=document.getElementById("button-pen")
+let pensize=document.getElementById("pen-slidearea")
+let tooltype=document.getElementById("tool-type")
+
+// when using not using both  earse or drwing 
+let ease_boolean=false
+let draw_boolean=false
+
+// canavs
+
